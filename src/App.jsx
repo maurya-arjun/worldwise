b7b89d1187678573d53a35c17489514929a8a74b
ARJUN KUMAR MARUYA <arjunkumarmaruya@ARJUNs-MacBook-Air.local>
2025-07-01 04:54:49 +0530

    css modules

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Pagenotfound from "./pages/Pagenotfound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<Pagenotfound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AppNav from "../components/AppNav";
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      App Layout
+    </div>
+  );
+}
+
+export default AppLayout;
