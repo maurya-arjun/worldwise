659ca9d4ffd62e47dabb2bd5dae742181e2dfaeb
ARJUN KUMAR MARUYA <arjunkumarmaruya@ARJUNs-MacBook-Air.local>
2025-07-03 08:03:57 +0530

    implement countries list

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from "./pages/Login";
 import Pagenotfound from "./pages/Pagenotfound";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:3001";
 
@@ -50,7 +51,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<div>Countries List</div>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<div>Form Page</div>} />
         </Route>
         <Route path="*" element={<Pagenotfound />} />
